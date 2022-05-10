1241698ba6c710914f371d818479851dbeb351b6
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2022-05-11 00:42:24 +0700

    fix loi giao dien QLNV

--- admin/QLNhanVien.php
+++ admin/QLNhanVien.php
@@ -19,10 +19,6 @@
             <div class="container-fluid px-4" style="margin-bottom: 20%;">
                 <div class="row my-5">
                     <h3 class="fs-4 mb-3">Quản lý nhân viên</h3>
-                    <form class="form-inline my-2 my-lg-0" method="GET" action="">
-                      <input class="form-control mr-1 sm-2 border-warning" type="text" name="key" placeholder="Bắt đầu tìm kiếm..." aria-label="Search" style="width:30%; float: right; margin:15px; margin-right:0;">
-                      <button class="btn btn-outline my-2 my-sm-0 mr-1" type="submit"></button>
-                    </form>
                     <div class="col">
                         <table class="table bg-white rounded shadow-sm mt-3  table-hover">
                             <thead>
