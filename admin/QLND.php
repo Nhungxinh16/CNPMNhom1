77ff4d5c23c16e8547d50b31eb66118710caa62e
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2022-05-16 20:51:20 +0700

    giao dien xuat file

---
+++ admin/QLND.php
@@ -0,0 +1,76 @@
+<?php
+    require("../config/constants.php");
+    require_once("check-admin.php");
+    $_SESSION["nav"] = "customers";
+    require('templates-admin/header.php');
+    if(isset($_SESSION["alert"])){
+        $message = $_SESSION["alert"];
+        unset($_SESSION["alert"]);
+        echo '<script>
+                alert("'.$message.'");
+            </script>
+        ';
+    }
+    $key='';
+    if(isset($_GET['key'])){
+        $key=$_GET['key'];
+    }
+?>
+            <div class="container-fluid px-4" style="margin-bottom: 20%;">
+                <div class="row my-5">
+                    <h3 class="fs-4 mb-3">Quản lý khách hàng</h3>
+                    <div class="col">
+                        <table class="table bg-white rounded shadow-sm  table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col" width="50">#</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Điện thoại</th>
+                                    <th scope="col">Giới tính</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $i = 1;
+                                    $sql = "select * from customers where is_admin = 0 and user_name like '%".$key."%'";
+                                    $rows = simpleQuery($sql);
+                                    foreach($rows as $row){
+                                        echo '
+                                            <tr>
+                                                <th scope="row">'.$i.'</th>
+                                                <td>'.$row["user_name"].'</td>
+                                                <td>'.$row["email"].'</td>
+                                                <td>'.$row["phone_number"].'</td>
+                                                <td>';
+                                                    if($row["gender"] == 0){
+                                                        echo "Nữ";
+                                                    }else{
+                                                        echo "Nam";
+                                                    }
+                                                echo '</td>
+                                            </tr>
+                                        ';
+                                        $i++;
+                                    }
+                                ?>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+          <div>
+            <form action="export-import.php" method="POST" enctype="multipart/form-data">
+                <button class="btn btn-dark" type="submit" name="export">Xuất file</button>
+            </form>
+        </div>
+<?php
+    include('templates-admin/footer.php')
+?>
+<html>
+<script language="javascript" type="text/javascript">
+  function confirmFuncc(){
+    return confirm("Bạn có chắc chắn muốn xóa không?");
+  }
+</script>
+</html>
